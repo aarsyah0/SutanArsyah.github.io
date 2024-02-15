d010bde1424da97b8b27ab027e25bb181386e8d4
aarsyah0 <arsasaja2@gmail.com>
2024-02-15 19:17:55 +0700

    routes,controller,view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+    <div>
+        <h1>Welcome to Our Website!</h1>
+        <p>This is the homepage of our website.</p>
+    </div>
+</body>
+</html>
